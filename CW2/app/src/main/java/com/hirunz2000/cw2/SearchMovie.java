3ac747e5ddf98e88da6aa1842ffe4e0911cad70f
Hirunz <54896502+Hirunz@users.noreply.github.com>
2021-04-13 09:52:31 +0530

    search activity is complete

--- CW2/app/src/main/java/com/hirunz2000/cw2/SearchMovie.java
+++ CW2/app/src/main/java/com/hirunz2000/cw2/SearchMovie.java
@@ -22,6 +22,7 @@ import java.util.Collections;
 public class SearchMovie extends AppCompatActivity {
     private static String LOG_TAG = SearchMovie.class.getSimpleName();
 
+    // array lists to store all movies and searched movies
     private ArrayList<Movie> searchedMovies =new ArrayList<>();
     private ArrayList<Movie> movies;
 
@@ -32,9 +33,11 @@ public class SearchMovie extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_movie);
 
+        //  remove actionbar
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+        // get all movies
         movies = database.getMovies();
 
     }
@@ -43,20 +46,37 @@ public class SearchMovie extends AppCompatActivity {
         EditText et = findViewById(R.id.search_EditText);
         String searchText = et.getText().toString().toLowerCase();
         Log.d(LOG_TAG, searchText);
+        // clear the searched movies before searching
         searchedMovies.clear();
-        for (Movie m: movies){
 
-            if (m.getTitle().toLowerCase().contains(searchText) ||
-                    m.getDirector().toLowerCase().contains(searchText) ||
-                    m.getActors().toLowerCase().contains(searchText)
-            ) {
-                Log.d(LOG_TAG, "movie found");
-                searchedMovies.add(m);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // find the movies which has the user input in movie title, director or actors
+                // then add it to the searched movies list
+                for (Movie m: movies){
+
+                    if (m.getTitle().toLowerCase().contains(searchText) ||
+                            m.getDirector().toLowerCase().contains(searchText) ||
+                            m.getActors().toLowerCase().contains(searchText)
+                    ) {
+                        Log.d(LOG_TAG, "movie found");
+                        searchedMovies.add(m);
+                    }
+
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // update the ui
+                        displayMovies();
+                    }
+                });
             }
+        }).start();
 
-        }
 
-        displayMovies();
     }
 
 
@@ -68,10 +88,17 @@ public class SearchMovie extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        // if there are no movies
                         if (searchedMovies == null || searchedMovies.isEmpty()){
                             Toast.makeText(getApplicationContext(),"No Movies Available !", Toast.LENGTH_SHORT).show();
                             return;
                         }
+                       /*
+                        * for each movie in the search
+                        * add the title to a textview
+                        * add textview to a card view
+                        * and display it.
+                        */
                         for (Movie m: searchedMovies){
                             CardView.LayoutParams cardParams = new  CardView.LayoutParams(CardView.LayoutParams.MATCH_PARENT, CardView.LayoutParams.MATCH_PARENT);
                             cardParams.setMargins(120,0,120,25);
@@ -92,10 +119,11 @@ public class SearchMovie extends AppCompatActivity {
                             tv.setTextSize(20);
                             tv.setTextColor(Color.parseColor("#000814"));
 
-
+                            // fonts
                             Typeface tf = ResourcesCompat.getFont(getApplicationContext(), R.font.noto_sans);
                             tv.setTypeface(tf);
                             tv.setGravity(Gravity.CENTER);
+                            tv.setPadding(10,10,10,10);
 
                             card.addView(tv);
 
